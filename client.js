d8fe56a422c2c30b71452eefda31b4ec1bab9a22
micstach <micstach@poczta.onet.pl>
2017-09-18 21:59:39 +0200

    add method added

--- client.js
+++ client.js
@@ -7,8 +7,24 @@ function test() {
     var client = new hello_proto.Greeter('127.0.0.1:50051', grpc.credentials.createInsecure());
 
     client.sayHello({name: "Michal"}, function(err, response) {
-        console.log('Greeting:', response.message);
+        if (err) {
+          console.log(err);
+        }
+
+        if (response) {
+          console.log('Greeting:', response.message);
+        }
     });
+
+    client.add({a: 1, b: 2}, function(err, response) {
+      if (err) {
+        console.log(err);
+      }
+
+      if (response) {
+        console.log('Add Value', response.value);
+      }
+  });
 }
 
 test();
--- server.js
+++ server.js
@@ -7,9 +7,16 @@ function sayHello(call, callback) {
     callback(null, {message: 'Hello my friend: ' + call.request.name});
 }
 
+function add(call, callback) {
+    callback(null, {value: call.request.a + call.request.b});
+}
+
 function main() {
     var server = new grpc.Server();
-    server.addProtoService(hello_proto.Greeter.service, {sayHello: sayHello});
+    server.addProtoService(hello_proto.Greeter.service, {
+	sayHello: sayHello,
+        add: add
+    });
     server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());
     server.start();
 }
